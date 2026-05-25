e47e64a4106d4d16f4b7a6488a587d46cc413983
samriddha poudel <poudelsamriddha46@gmail.com>
2026-05-25 18:47:19 +0545

    added the input validation if input field is empty

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const dictionaryAPIurl=`https://api.dictionaryapi.dev/api/v2/entries/en/`;
 const translateApiurl='https://api.mymemory.translated.net/get';
+
+let lastSearchedWord='';
+
+async function searchwords(){
+
+
+    const wordInput = document.getElementById('wordinput');
+    const languageSelect= document.getElementById('language-select');
+    const resultDiv = document.getElementById('result');
+    const word = wordInput.value.trim();
+    const targetLanguage = languageSelect.value;
+
+
+    if(!word){
+        resultDiv.textContent = 'Please enter a word or sentence';
+        return;
+    }
+
+}
