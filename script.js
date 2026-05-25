d0de7f77c47e8ce4bab563b9c8ff1a52d05efafc
samriddha poudel <poudelsamriddha46@gmail.com>
2026-05-25 21:14:02 +0545

    added dictionary fetch with result display

--- script.js
+++ script.js
@@ -18,4 +18,67 @@ async function searchwords(){
         return;
     }
 
+
+    if(word !== lastSearchedWord){
+        lastSearchedWord = word;
+        resultDiv.textContent= 'Searching....';
+    
+
+    try{
+        const translateResponse = await fetch(`${translateApiurl}?q=${encodeURIComponent(word)}&langpair=en|${targetLanguage}`);
+
+        if(!translateResponse.ok){
+            throw new Error(`HTTP error! status: ${translateResponse.status}`);
+
+        }
+
+
+
+        const translateData = await translateResponse.json();
+
+
+        const dictionaryResponse =  await fetch(dictionaryAPIurl + encodeURIComponent(word));
+        let dictionaryData = [];
+
+        if(dictionaryResponse.ok){
+            dictionaryData = await dictionaryResponse.json();
+        }
+
+        let result = `word/phrase: ${word}\n`;
+        result += `Translation (${getLanguageName(targetLanguage)}): ${translateData.responseData.translatedText}\n\n `;
+
+
+
+        if(dictionaryData && dictionaryData.length > 0){
+          
+            dictionaryData[0].meanings.forEach((meaning, index) => {
+                result += `Meaning${index + 1} (${meaning.partOfSpeech}):\n`; 
+                meaning.definitions.forEach((def, defIndex) => {
+                    result += `${defIndex + 1}. ${def.definition}\n`;
+                    if(def.example){
+                        result += ` Example: ${def.example}\n`;
+                    }
+
+
+                });
+                result += '\n';
+                 
+            });
+        }else{
+            result += 'No additional definitions found on this word.\n';
+        }
+        resultDiv.textContent = result;
+    }
+    catch(error){
+        console.error('Error:', error);
+        resultDiv.textContent =`An error occured: ${error.message}`;
+    }   
+
+}else{
+        location.reload();
+    }
+   
+
+
 }
+
